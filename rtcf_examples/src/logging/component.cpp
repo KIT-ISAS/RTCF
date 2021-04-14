1b16033b662a488f93b03c9cf50e943b21114de6
Michael Fennel <michael.fennel@kit.edu>
2021-04-14 16:36:52 +0200

    added example for logging

---
+++ rtcf_examples/src/logging/component.cpp
@@ -0,0 +1,44 @@
+#include "component.hpp"
+
+#include <iostream>
+#include <rtt/Component.hpp>
+
+Logging::Logging(std::string const& name) : TaskContext(name) { std::cout << "Logging constructed !" << std::endl; }
+
+bool Logging::configureHook() {
+    // non real-time logging
+    NON_RT_DEBUG("Test non-RT debug");
+    NON_RT_DEBUG_STREAM("Test non-RT debug stream with number " << 12345);
+    NON_RT_INFO("Test non-RT info");
+    NON_RT_INFO_STREAM("Test non-RT info stream with number " << 12345);
+    NON_RT_WARN("Test non-RT warn");
+    NON_RT_WARN_STREAM("Test non-RT warn stream with number " << 12345);
+    NON_RT_ERROR("Test non-RT error");
+    NON_RT_ERROR_STREAM("Test non-RT error stream with number " << 12345);
+    NON_RT_FATAL("Test non-RT fatal");
+    NON_RT_FATAL_STREAM("Test non-RT fatal stream with number " << 12345);
+
+    return true;
+}
+
+bool Logging::startHook() { return true; }
+
+void Logging::updateHook() {
+    // real-time logging
+    RT_DEBUG("Test RT debug");
+    RT_DEBUG_STREAM("Test RT debug stream with number " << 12345);
+    RT_INFO("Test RT info");
+    RT_INFO_STREAM("Test RT info stream with number " << 12345);
+    RT_WARN("Test RT warn");
+    RT_WARN_STREAM("Test RT warn stream with number " << 12345);
+    RT_ERROR("Test RT error");
+    RT_ERROR_STREAM("Test RT error stream with number " << 12345);
+    RT_FATAL("Test RT fatal");
+    RT_FATAL_STREAM("Test RT fatal stream with number " << 12345);
+}
+
+void Logging::stopHook() {}
+
+void Logging::cleanupHook() {}
+
+ORO_CREATE_COMPONENT(Logging)
